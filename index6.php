7006f4355380849c2ef4b53da4cb791b8f26843a
sushmitha sudarsan <ss3598@afsconnect1.njit.edu>
2017-11-25 20:40:01 -0500

    namespaces index6.php

--- index6.php
+++ index6.php
@@ -6,6 +6,7 @@ define('DATABASE', 'ss3598');
 define('USERNAME', 'ss3598');
 define('PASSWORD', 'owlnerZ1g');
 define('CONNECTION', 'sql1.njit.edu');
+//namespace
   class dbConn{
 
     protected static $db;
